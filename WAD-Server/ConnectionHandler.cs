87451fc8ed7822d2e7fc8f27ff0d7f592a004319
seanmarcus <s.e.a.n.marcus@hotmail.com>
2018-01-08 20:47:19 +0800

    8/JAN/2018 - Added functions

---
+++ WAD-Server/ConnectionHandler.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WAD_Server
+{
+    class ConnectionHandler
+    {
+        private Socket client;
+        private NetworkStream ns;
+        private StreamReader reader;
+        private StreamWriter writer;
+        private static int connections = 0;
+        private Form1 f;
+        
+        // Current way to keep track of multi socket (can be changed to dict<int, socket>)
+        public static ArrayList arrSocket = new ArrayList();
+
+        public ConnectionHandler(Socket client, Form1 f)
+        {
+            this.client = client;
+            this.f = f;
+        }
+
+        public void HandleConnection(Object state)
+        {
+            try
+            {
+                ns = new NetworkStream(client);
+                reader = new StreamReader(ns);
+                writer = new StreamWriter(ns);
+                connections++;
+                arrSocket.Add(client);
+
+                f.SetText("New client accepted : " + connections + " active connections");
+
+                string input;
+                while (true)
+                {
+                    input = reader.ReadLine();
+
+                    if (input.ToLower() == "terminate")
+                        break;
+                    f.SetText("Client >> " + input);
+                }
+                ns.Close();
+                client.Close();
+                connections--;
+                f.SetText("Client disconnected : " + connections + " active connections");
+            }
+            catch (Exception)
+            {
+                connections--;
+                f.SetText("Client disconnected : " + connections + " active connections");
+            }
+        }
+    }
+}
--- WAD-Server/Form1.cs
+++ WAD-Server/Form1.cs
@@ -4,17 +4,69 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace WAD_Server
 {
     public partial class Form1 : Form
     {
+        Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        // Declare delegate
+        public delegate void SetTextCallback(string msg);
+
         public Form1()
         {
             InitializeComponent();
+
+            // Starts the server in the background
+            runServer();
         }
+
+        // Code to start the server
+        #region runServer()
+        async void runServer()
+        {
+            int port = 7000;
+            IPEndPoint endpoint = new IPEndPoint(IPAddress.Any, port);
+            server.Bind(endpoint);
+            server.Listen(10);
+            SetText("Waiting for clients on port " + port);
+            await Task.Run(() =>
+            {
+                while (true)
+                {
+                    try
+                    {
+                        Socket client = server.Accept();
+                        ConnectionHandler handler = new ConnectionHandler(client, this);
+                        ThreadPool.QueueUserWorkItem(new WaitCallback(handler.HandleConnection));
+                    }
+                    catch (Exception)
+                    {
+                        SetText("Connection falied on port " + port);
+                    }
+                }
+            });
+        }
+        #endregion
+
+        // To prevent cross-threading and set text
+        #region setText() function
+        public void SetText(string msg)
+        {
+            if (this.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke(d, msg);
+                return;
+            }
+            //txtResult.Text = msg;
+        }
+        #endregion
     }
 }
