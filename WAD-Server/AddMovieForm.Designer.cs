464a6bd0a463df98007ef69cd79394fe3d3c3673
seanmarcus <s.e.a.n.marcus@hotmail.com>
2018-01-09 10:40:47 +0800

    Updated dropdownlist

--- WAD-Server/AddMovieForm.Designer.cs
+++ WAD-Server/AddMovieForm.Designer.cs
@@ -77,6 +77,7 @@
             // 
             // cbType
             // 
+            this.cbType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbType.FormattingEnabled = true;
             this.cbType.Items.AddRange(new object[] {
             "Action",
